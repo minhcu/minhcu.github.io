7f8cdced021c4609b0f35d67065e2b500963b2c5
minhcu <minhdong1831999@gmail.com>
2021-04-05 17:12:25 +0700

    update

--- side-project/baitap/btbuoi10/btbuoi10.js
+++ side-project/baitap/btbuoi10/btbuoi10.js
@@ -18,6 +18,40 @@ const test2PartA = function showMmddyyyy () {
 // 3
 const test3 = function numberCheck () {
     let number = document.getElementById('number').value;
-    
     document.getElementById('result').innerHTML = number;
+}
+
+// 7
+const test7 = function javaCheck () {
+    const string = document.getElementById('string').value;
+    const java = string.indexOf('java');
+    if (java > 0) {
+        document.getElementById('test7Result').innerHTML = 'Chuỗi có chứa \'Java\'';
+    }
+    else {
+        document.getElementById('test7Result').innerHTML = 'Chuỗi không chứa \'Java\'';
+    }
+}
+
+// 8
+const test8 = function monthCheck () {
+    const number = document.getElementById('monthNumber').value - 1;
+    const month = ['tháng một', 'tháng hai', 'tháng ba', 'tháng bốn', 'tháng năm', 'tháng sáu', 'tháng bảy', 'tháng tám', 'tháng chín', 'tháng mười', 'tháng mười một', 'tháng mười hai']
+    document.getElementById('monthName').innerHTML= month[number];
+}
+
+// 10
+const test10 = function integerCheck () {
+    const startNumber = document.getElementById('startNumber').value;
+    const endNumber = document.getElementById('endNumber').value;
+    for (startNumber; startNumber <= endNumber; startNumber++) {
+        for (i=2; i <= startNumber; i++) {
+            if (startNumber % i == 0) {
+                document.getElementById('monthName').innerHTML= startNumber;
+            }
+            else {
+                document.getElementById('monthName').innerHTML= 'Không có số nguyên tố';
+            }
+        }
+    }
 }
