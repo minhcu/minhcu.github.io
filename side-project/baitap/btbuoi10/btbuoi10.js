b0b4f8a6def3cfe9a8ed60ec65ee06e37e160ea4
minhcu <minhdong1831999@gmail.com>
2021-04-07 16:14:03 +0700

    done js

--- side-project/baitap/btbuoi10/btbuoi10.js
+++ side-project/baitap/btbuoi10/btbuoi10.js
@@ -6,11 +6,11 @@ const test1 = function showDate() {
 // 2
 const test2PartA = function showMmddyyyy () {
     const date = new Date();
-    const mm = date.getMonth()+1;
+    const mm = date.getMonth() + 1;
     const dd = date.getDate();
     const yyyy = date.getFullYear();
     document.getElementById('mm-dd-yyyy').innerHTML = mm + '-' + dd + '-' + yyyy;
-    document.getElementById('mm/dd/yyy  y').innerHTML = mm + '/' + dd + '/' + yyyy;
+    document.getElementById('mm/dd/yyyy').innerHTML = mm + '/' + dd + '/' + yyyy;
     document.getElementById('dd-mm-yyyy').innerHTML = dd + '-' + mm + '-' + yyyy;
     document.getElementById('dd/mm/yyyy').innerHTML = dd + '/' + mm + '/' + yyyy;
 }
@@ -41,37 +41,50 @@ const test3 = function numberCheck () {
     }
 }
 
+// 4
+const test4 = function stringReplace () {
+    const test4Text = document.getElementById('test4Text').value;
+    let test4Result = '';
+    for (i = 0; i <= test4Text.length - 1; i++) {
+        test4Result = test4Result + String.fromCharCode(test4Text.charCodeAt(i) + 1);
+    }
+    document.getElementById('test4Result').innerHTML = test4Result;
+}
+
 // 5
 const test5 = function stringCreator () {
     let test5Text = document.getElementById('test5Text').value;
-    if (test5Text.length % 2 == 0) {
+    if (test5Text.length < 3) {
+        document.getElementById('test5Result').innerHTML = 'Nhập thêm pls';
+    }
+    else if (test5Text.length % 2 == 0) {
         document.getElementById('test5Result').innerHTML = 'Chuỗi chẵn rồi, nhập lại pls';
     }
     else {
-        document.getElementById('test5Result').innerHTML = 'Chuỗi lẻ rồi, nhưng mà chưa code tiếp';
+        const startPosition = (test5Text.length - 1) / 2 - 1;
+        document.getElementById('test5Result').innerHTML = test5Text.substr(startPosition,3);;
     }
 }
 
 // 6
-const test6 = function duplicateTest () {
-    const test6Input = document.getElementById('test6Input').value;
-    const array = test6Input.split(' ');
-    count = {};
-    max = 0;
-    test6Result;
-    array.forEach(element => {
-        if (count[element]) {
-            count = 1;
+const test6 = function duplicateCheck () {
+    test6Input = document.getElementById('test6Input').value.split(' ');
+    counter = {};
+    duplicateNumber = 0;
+    test6Result = '';
+    test6Input.forEach(element => {
+        if (counter[element]) {
+            counter[element] += 1;
         }
         else {
-            count++
+            counter[element] = 1;
         }
-        if (count[element] > max) {
-            max = count[element];
+        if (counter[element] > duplicateNumber) {
             test6Result = element;
+            duplicateNumber = counter[element];
         }
     });
-    document.getElementById('test6Result').innerHTML = test6Result;
+    document.getElementById('test6Result').innerHTML = 'Số ' + test6Result + ' (' + duplicateNumber + ' lần)';
 }
 
 // 7
@@ -91,6 +104,21 @@ const test8 = function monthCheck () {
     const number = document.getElementById('monthNumber').value - 1;
     const month = ['tháng một', 'tháng hai', 'tháng ba', 'tháng bốn', 'tháng năm', 'tháng sáu', 'tháng bảy', 'tháng tám', 'tháng chín', 'tháng mười', 'tháng mười một', 'tháng mười hai']
     document.getElementById('monthName').innerHTML= month[number];
+}
+
+// 9
+const test9 = function lengthCheck () {
+    const test9Text = document.getElementById('test9Text').value;
+    const test9Array = test9Text.split(' ');
+    let test9Result = '';
+    let a = 0;
+    test9Array.forEach(element => {
+        if (element.length > a) {
+            a = element.length;
+            test9Result = element;
+        }
+    });
+    document.getElementById('test9Result').innerHTML = test9Result;
 }
 
 // 10
