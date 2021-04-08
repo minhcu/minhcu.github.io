cf65e37a5d2681c6b291b642327e09eb311092cc
minhcu <minhdong1831999@gmail.com>
2021-04-09 00:32:43 +0700

    update

--- side-project/baitap/quanlyanh/javascript.js
+++ side-project/baitap/quanlyanh/javascript.js
@@ -1,10 +1,40 @@
-// Validity
 
-const validityCheck = function validityCheck() {
-    let name = document.getElementById("name");
-    let category = document.getElementById("category");
-    if (typeof name.substr(0,1) == "number") {
-        document.getElementById("warning-name").innerHTML = "*Tên không hợp lệ";
-    }
-    
+const formInput = document.getElementsByTagName("form")[0];
+const itemName = document.getElementById("input__item-name");
+const itemCategory = document.getElementById("input__category");
+const itemImage = document.getElementById("input__image");
+// Preview Image
+const imagePreview = function (event) {
+  document.getElementById("input__image-warning").innerHTML = "";
+  document.getElementById("input__image--chosen").src = URL.createObjectURL(event.target.files[0]);
 }
+// Validity & Get file
+const validity = formInput.addEventListener("submit", function (event) {
+  if (!itemName.validity.valid) {
+    document.getElementById("input__name-warning").innerHTML = "Không hợp lệ";
+    event.preventDefault();
+  }
+  else {
+    document.getElementById("input__name-warning").innerHTML = "";
+  }
+  if (!itemCategory.validity.valid) {
+    document.getElementById("input__category-warning").innerHTML = "Không hợp lệ";
+    event.preventDefault();
+  }
+  else {
+    document.getElementById("input__category-warning").innerHTML = "";
+  }
+  if (!itemImage.validity.valid) {
+    document.getElementById("input__image-warning").innerHTML = "Không hợp lệ";
+    event.preventDefault();
+  }
+  
+});
+// Show Data
+const submit = function () {
+  if ((itemName.validity.valid) & (itemCategory.validity.valid) & (itemImage.validity.valid)) {
+    let formInput = {};
+    formInput.append()
+  }
+}
+
