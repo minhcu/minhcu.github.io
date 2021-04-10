a3de1539c06524a750ae51eadd29f48a15626561
minhcu <minhdong1831999@gmail.com>
2021-04-11 00:57:46 +0700

    update

--- side-project/baitap/quanlyanh/javascript.js
+++ side-project/baitap/quanlyanh/javascript.js
@@ -1,41 +1,77 @@
-
-const formInput = document.getElementsByTagName("form")[0];
+// Create Entity Set
+const inputSet = [
+  {
+    itemnumber: "1",
+    itemname: "1",
+    itemcategory: "1",
+    itemimage: "blob:http://127.0.0.1:5500/1a92687f-2466-4ff0-8b4c-90f13e3a3b3a",
+  }, 
+  {
+    itemnumber: "2",
+    itemname: "2",
+    itemcategory: "2",
+    itemimage: "2",
+  }
+];
+// Create Entity
+let input = {
+  key: "1",
+  name: "1",
+  category: "1",
+  url: "1",
+};
 // Preview Image
 const imagePreview = function (event) {
-  document.getElementById("input__image-warning").innerHTML = "";
-  document.getElementById("input__image--chosen").src = URL.createObjectURL(event.target.files[0]);
+  document.getElementById("input__image__warning").innerHTML = "";
+  document.getElementById("image-preview").src = URL.createObjectURL(event.target.files[0]);
 }
-// Validity & Get file
-const validity = formInput.addEventListener("submit", function (event) {
-  const itemName = document.getElementById("input__item-name");
-  const itemCategory = document.getElementById("input__category");
-  const itemImage = document.getElementById("input__image");
+// Validate
+const submit = function inputValue (event) {
+  const itemName = document.getElementById("input-name");
+  const itemCategory = document.getElementById("input-category");
+  const itemImage = document.getElementById("input-image");
+  // Validate name
   if (!itemName.validity.valid) {
-    document.getElementById("input__name-warning").innerHTML = "Không hợp lệ";
+    document.getElementById("input__item-name__warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
   }
   else {
-    document.getElementById("input__name-warning").innerHTML = "";
+    document.getElementById("input__item-name__warning").innerHTML = "";
   }
+  // Validate Category
   if (!itemCategory.validity.valid) {
-    document.getElementById("input__category-warning").innerHTML = "Không hợp lệ";
+    document.getElementById("input__item-category__warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
   }
   else {
-    document.getElementById("input__category-warning").innerHTML = "";
+    document.getElementById("input__item-category__warning").innerHTML = "";
   }
+  // Validate Image
   if (!itemImage.validity.valid) {
-    document.getElementById("input__image-warning").innerHTML = "Không hợp lệ";
+    document.getElementById("input__image__warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
   }
-  if ((itemName.validity.valid) & (itemCategory.validity.valid) & (itemImage.validity.valid)) {
-    localStorage.setItem("itemName", itemName.value);
-    localStorage.setItem("itemCategory", itemCategory.value);
-    localStorage.setItem("itemImage", document.getElementById("input__image--chosen").src);
-    document.getElementById("input__image--chosen").src = "";
-    document.getElementById("input__form").reset();
-    document.getElementById("output__item-name").innerHTML = localStorage.getItem("itemName");
-    document.getElementById("output__item-category").innerHTML= localStorage.getItem("itemCategory");
-    document.getElementById("output__item-image").src= localStorage.getItem("itemImage");
-  }  
-});
+}
+// Create Tabel
+for (i = 0; i < inputSet.length; i++) {
+  // Create output-container
+  const row = document.createElement("div");
+  row.setAttribute("class","output__content");
+  row.setAttribute("id",inputSet[i].itemnumber);
+  document.getElementById("output").appendChild(row);
+  // Create Output Items
+  function setID(element,list) {
+    for (x = 0; x < 5; x++) {
+      if (x == 3) {
+        cel = cell = document.createElement("img");
+      }
+      else {
+        cell = document.createElement("p");
+      }
+      cell.setAttribute("class","output-items");
+      cell.setAttribute(element,inputSet[i].itemnumber + "_" + list[x]);
+      row.appendChild(cell);
+    }
+  };
+  setID("id",["itemnumber","itemname","itemcategory","itemimage","itemaction"]);
+}
