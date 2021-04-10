17b54f05407a2924a7271abb2dd61a1b68c3bfdd
minhcu <minhdong1831999@gmail.com>
2021-04-10 21:56:55 +0700

    update

--- side-project/baitap/quanlyanh/javascript.js
+++ side-project/baitap/quanlyanh/javascript.js
@@ -10,13 +10,16 @@ const validity = formInput.addEventListener("submit", function (event) {
   const itemName = document.getElementById("input__item-name");
   const itemCategory = document.getElementById("input__category");
   const itemImage = document.getElementById("input__image");
-  if (!itemName.validity.valid) {
+  console.log(itemName.value.substr(0,1));
+  // Validate Name
+  if ((!itemName.validity.valid) || (parseInt(itemName.value.substr(0,1)))) {
     document.getElementById("input__name-warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
   }
   else {
     document.getElementById("input__name-warning").innerHTML = "";
   }
+  // Validate Category
   if (!itemCategory.validity.valid) {
     document.getElementById("input__category-warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
@@ -24,18 +27,15 @@ const validity = formInput.addEventListener("submit", function (event) {
   else {
     document.getElementById("input__category-warning").innerHTML = "";
   }
+  // Validate Image
   if (!itemImage.validity.valid) {
     document.getElementById("input__image-warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
   }
+  // Get Content
   if ((itemName.validity.valid) & (itemCategory.validity.valid) & (itemImage.validity.valid)) {
-    localStorage.setItem("itemName", itemName.value);
-    localStorage.setItem("itemCategory", itemCategory.value);
-    localStorage.setItem("itemImage", document.getElementById("input__image--chosen").src);
-    document.getElementById("input__image--chosen").src = "";
-    document.getElementById("input__form").reset();
-    document.getElementById("output__item-name").innerHTML = localStorage.getItem("itemName");
-    document.getElementById("output__item-category").innerHTML= localStorage.getItem("itemCategory");
-    document.getElementById("output__item-image").src= localStorage.getItem("itemImage");
-  }  
+    document.getElementById("output__item-name").innerHTML = itemName.value;
+    document.getElementById("output__item-category").innerHTML = itemCategory.value;
+    document.getElementById("output__item-image").src = document.getElementById("input__image--chosen").src;
+  }
 });
