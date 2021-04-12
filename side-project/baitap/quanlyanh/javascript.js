18caf8de7eedc6ad0264ec067ec1183f0a2eb854
minhcu <minhdong1831999@gmail.com>
2021-04-13 00:15:24 +0700

    update

--- side-project/baitap/quanlyanh/javascript.js
+++ side-project/baitap/quanlyanh/javascript.js
@@ -2,6 +2,8 @@
 const inputSet = [];
 // Create Entity
 let input = {};
+
+const button = document.getElementById("btn-1").cloneNode(true);
 // Create Table
 function addElement(array) {
   for (i = 0; i < array.length; i++) {
@@ -13,8 +15,8 @@ function addElement(array) {
   // Create Output Items
   function setID(element,idList,objValue) {
     objValue[0] = document.createTextNode(i+1);
-    objValue[3] = document.createTextNode(inputSet[i].image);
-    objValue[4] = document.createTextNode("button");
+    // objValue[3] = document.createTextNode(inputSet[i].image);
+    // objValue[4] = document.createTextNode("button");
     for (x = 0; x < 5; x++) {
         cell = document.createElement("div");
         row.appendChild(cell);
@@ -23,21 +25,66 @@ function addElement(array) {
         cell.appendChild(objValue[x]);
       }
     }
-  setID("id",["number","name","category","image","action"],[i+1,inputSet[i].name,inputSet[i].category,inputSet[i].image,"button"]);
+  setID("id",["number","name","category","image","action"],[i+1,inputSet[i].name,inputSet[i].category,inputSet[i].image,button]);
   };
 }
-// Preview Image
-const imagePreview = function (event) {
-  document.getElementById("input__image__warning").innerHTML = "";
-  document.getElementById("image-preview").src = URL.createObjectURL(event.target.files[0]);
-}
+// // Preview Image - OLD
+// const imagePreview = function (event) {
+//   document.getElementById("input__image__warning").innerHTML = "";
+//   document.getElementById("image-preview").src = URL.createObjectURL(event.target.files[0]);
+// }
+
+// Preview Image - NEW
+const previewImage = document.getElementById("input-image");
+const displayPreview = document.getElementById("image-preview");
+previewImage.addEventListener("change", function () {
+    let inputFile = this.files[0];
+    if (inputFile.type.indexOf('image') < 0) {
+      return;
+    }
+    const whyFileReader = new FileReader();
+    whyFileReader.onload = function() {
+      displayPreview.src = whyFileReader.result;
+      // convertImage(previewImage);
+    }
+    whyFileReader.readAsDataURL(inputFile);
+  }
+);
+
+// const convertImage = function convertImageData(img) {
+//   const canvas = document.createElement("canvas");
+//   canvas.width = img.width;
+//   canvas.height = img.height;
+
+//   let ctx = canvas.getContext("2d");
+//   ctx.drawImage(img, 0, 0);
+
+//   const dataURL = canvas.toDataURL("image/png");
+//   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Lock Category
 const selectedValue = function update() {
   let select = document.getElementById("input-category");
   for (i = 0; i < 4; i++) {
     select.options[i].removeAttribute("selected");
   }
   select.options[select.selectedIndex].setAttribute("selected","");
-} 
+}
+
 // Validate & Create Table
 const submit = function inputValue (event) {
   const itemName = document.getElementById("input-name");
@@ -60,7 +107,7 @@ const submit = function inputValue (event) {
     document.getElementById("input__item-category__warning").innerHTML = "";
   }
   // Validate Image
-  if (!itemImage.validity.valid) {
+  if ((!itemImage.validity.valid) && (file.type.indexOf('image') < 0)) {
     document.getElementById("input__image__warning").innerHTML = "Không hợp lệ";
     event.preventDefault();
   }
@@ -69,7 +116,7 @@ const submit = function inputValue (event) {
       key: Math.random(),
       name: itemName.cloneNode(true),
       category: itemCategory.cloneNode(true),
-      image: itemImage.value,
+      image: displayPreview.cloneNode(true),
     }
     input.name.setAttribute("disabled","");
     input.category.setAttribute("disabled","");
